40f1dda89f32e57dd66095f3a56fefc809621110
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-10 04:17:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,12 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    "Fi"
-  if int % 3 ==  0 
+    "FizzBuzz"
+  elsif int % 3 ==  0 
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
+  else 
+    "nil"
 end
 end
